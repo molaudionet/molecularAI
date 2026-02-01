85efce269bf9adb856234e00d1dfad01cbbfbd9c
Jianping Zhou <drzhou68@gmail.com>
2026-02-01 03:25:58 -0800

    modified bio50k.sh

--- BBBP_TOX21_BIO50K/bio50k.sh
+++ BBBP_TOX21_BIO50K/bio50k.sh
@@ -1,3 +1,3 @@
-python run.py fit --config configs/bioactive_desc.yaml --outdir runs/bioactive_desc
-python run.py fit --config configs/bioactive_audio.yaml --outdir runs/bioactive_audio
-python run.py fit --config configs/bioactive_fuse.yaml --outdir runs/bioactive_fuse
+python run.py fit --config configs/bioactive_desc.yaml --limit 200 --outdir runs/bioactive_desc
+python run.py fit --config configs/bioactive_audio.yaml --limit 200 --outdir runs/bioactive_audio
+python run.py fit --config configs/bioactive_fuse.yaml --limit 200 --outdir runs/bioactive_fuse
